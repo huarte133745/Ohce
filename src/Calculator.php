a3a5e4d2706a45a5133118ee560f2058c8b9ea2c
huarte133745 <huarte.133745@e.unavarra.es>
2023-06-16 00:48:52 +0000

    Refactorización: Producción devuelve 1

--- src/Calculator.php
+++ src/Calculator.php
@@ -4,12 +4,12 @@ namespace Deg540\PHPTestingBoilerplate;
 
 class Calculator
 {
-    function add(int $number1, int $number2): int
+    public function add(int $number1, int $number2): int
     {
         return 1;
     }
 
-    function multiply(int $number1, int $number2): int
+    public function multiply(int $number1, int $number2): int
     {
         return 0;
     }
